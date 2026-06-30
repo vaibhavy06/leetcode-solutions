966eacab5478fd2e4a59073655c19939fac4e69a
Vaibhav Yadav <yadavvaibhav062006@gmail.com>
2026-06-30 16:05:38 +0530

    Time: 0 ms (100%), Space: 8.8 MB (92.51%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        int end = s.length() - 1;
+        int end = s.length() - 1; // int end = s.length() - 1;
 
-        while(end >= 0 && s[end] == ' '){
+        while(end >= 0 && s[end] == ' '){ // while( end >= 0 && s[end] == ''){ end  --}
             end--;
         }
 
-        int start = end;
+        int start = end; // int s
         while(start >= 0 && s[start] != ' '){
             start --;
         }
